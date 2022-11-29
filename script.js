7f7a9b81de24f0352a095a85e77a7225cddfc11a
Your Name <you@example.com>
2022-11-29 12:09:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+"use strict"
+
+// console.log('35' + - "22");
+
+// console.log('35' * "22");
+
+// console.log('558' > 22++);
+
+/* let userCounter = 0;
+ let newUser = userCounter++;
+
+ console.log(newUser);
+ console.log(userCounter); */
+
+//  console.log(!false && 11 || 18 && !'');
+
+/* let num = 0;
+ console.log(num ?? "not name") */
+
+
+
+// ====================================================
+
+/* if (1 === '1') {
+    console.log("Правда");
+} */
+
+/* let message = (92 > '11' && 58 < 100) ? "Правда" : "Брехня";
+console.log(message); */
+
+
+// ====================================================
+
+
+/* let num = 8;
+
+while (num) {
+    console.log(num);
+    num--
+} */
+
+
+/* for (let num = 0; num <= 5; num++) {
+    console.log(num);
+}  */
+
+
+/* for (let num = 0; num < 3; num++) {
+    console.log(`Число: ${num}`);
+}
+
+
+let num = 0;
+
+do {
+    console.log(`Число: ${num}`);
+    num++;
+} while (num < 3);  */
+
+
+
+/* first: for (let num = 0; num < 2; num++) {
+    for (let size = 0; size < 3; size++) {
+
+        if (size == 1) break first;
+        console.log(size);
+    }
+} */
+
+
+// ====================================================
+
+/* let numOne = Math.round((1.005 + Number.EPSILON) * 100) / 100;
+console.log(numOne); */
+
+
+/* let value = parseFloat("135.58px");
+console.log(value); */
+
+/* let value = 58 + "Фрилансер";
+
+if (isNaN(value)) {
+    console.log('Результат виразу NaN');
+} */
+
+// ====================================================
+
+/* let text = "фрилансер";
+console.log(text[5]) */
+
+/* let text = "фрилансер";
+console.log(text.toUpperCase()) */
+
+/* let text = "фрилансер";
+console.log(text.slice(3, 6)) */
+
